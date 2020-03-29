16bc8d1f529c036d026d82b495b38e4f44543b0a
Hamish Coates <h.coates.developer@gmail.com>
2020-03-29 21:14:52 +0100

    csdc

--- deployment.tf
+++ deployment.tf
@@ -24,14 +24,6 @@ resource "digitalocean_droplet" "moonshotlabs_paas" {
   ssh_keys = [digitalocean_ssh_key.ssh_keys_client.id]
 }
 
-# output "moonshotlabs_paas_ip_addr" {
-#   value = {
-#     for instance in digitalocean_droplet.moonshotlabs_paas:
-#     instance.id => instance.ipv4_address
-#   }
-#   description = "The IP addresses of the deployed instances, paired with their IDs."
-# }
-
 resource "digitalocean_volume_attachment" "moonshotlabs_attached_volume" {
   droplet_id = digitalocean_droplet.moonshotlabs_paas.id
   volume_id  = digitalocean_volume.moonshotlabs_data.id
